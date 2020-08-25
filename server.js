12e6e57228d23ba1dbd4b69eef5052a8c6748da6
alex yanovich <alexyanovich@hotmail.com>
2020-08-25 12:35:07 -0400

    try uploading again

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
