85562cf15af9f5a9f3dda034eec1fc872da03c83
alex yanovich <alexyanovich@hotmail.com>
2020-08-25 22:05:08 -0400

    finishing touches

--- views/Index.jsx
+++ views/Index.jsx
@@ -15,12 +15,16 @@ class Index extends React.Component {
                 </nav>
                 <h3>Lets build your first brew! </h3>
 
+                <h3>Use this website as a guide to build your very first home brewery! Start by clicking a topic you want to learn more about or if you're lost click the brewing process tab!</h3>
+                <img id="store" src= "https://images.squarespace-cdn.com/content/v1/56eb44e7a3360c45493cd1ff/1468180051141-RBMQ2JX2DROHQ60V1FTB/ke17ZwdGBToddI8pDm48kDRipwOq3v44bTldlIoZZ-97gQa3H78H3Y0txjaiv_0fQZQHeq0CwZe2fIqUUdZnbwVtqe4SbB55k1DuUOwTBBmXgeHCFdeoz64T3if0EP9-eoqS5JAWrnb1436X_b_yVRx0TKp4jCW7sB_BGURRbuWR-qnQeGSdU98--sqBiP7esVwk1m85ElTBgxmdvCq-Kw/IMG_20160708_153902121.jpg"></img>
+
+
                 <ul>
                     {
                         brew.map((Brew)=>{
                             return (
                                 <li>
-                                    <a href={`brew/${Brew._id}`}></a>  <img id="store" src= "https://i2.wp.com/kegandbarrelhomebrewsupplies.com/wp-content/uploads/2017/11/img_3541.jpg?resize=1180%2C600&ssl=1"></img>
+                                    <a href={`brew/${Brew._id}`}></a>  
                                     brand: {Brew.brand}<br />
                                     batch size: {Brew.batchSize}<br />
                                     material: {Brew.material}<br />
