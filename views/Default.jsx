4fea1f82e81be3aed385eb0acda1ba62b62f3ad1
alex yanovich <alexyanovich@hotmail.com>
2020-08-25 20:22:41 -0400

    do not change

--- views/Default.jsx
+++ views/Default.jsx
@@ -12,7 +12,7 @@ class Default extends React.Component {
                     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
                     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
                     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-                    <link rel="stylesheet" type="text/css" href="styles.css"></link>
+                    <link rel="stylesheet" type="text/css" href="/styles.css"></link>
                     <title>Yanks Home Brew Guide</title>
                 </head>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -34,7 +34,7 @@ class Default extends React.Component {
 </nav>
                 <body> 
                     
-                    <img id= "banner" src="https://www.cohoferry.com/img/pages/FWBF-banner.jpg" class="img-fluid" alt="Responsive image"></img>
+                    <img id="banner"  src="https://www.cohoferry.com/img/pages/FWBF-banner.jpg" class="img-fluid" alt="Responsive image"></img>
                     
                     {this.props.children}
                     <footer>
